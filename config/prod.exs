6c226b8c1788e7011083ecd0d186ddde8849ba7c
im <poson_hemp@yahoo.co.jp>
2020-12-25 19:28:55 +0900

    mix format

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,8 @@ config :sample_app, SampleAppWeb.Endpoint,
   url: [scheme: "https", host: "local-playground-no-ecto.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
-  # secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
