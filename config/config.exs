e81f0243a50d5fe069a5e579767196f50673ff04
im <poson_hemp@yahoo.co.jp>
2020-12-28 17:19:48 +0900

    re-prepare git hooks simply

--- config/config.exs
+++ config/config.exs
@@ -29,22 +29,19 @@ import_config "#{Mix.env()}.exs"
 
 # Set git_hooks
 if Mix.env() != :prod do
-
   config :git_hooks,
     auto_install: true,
     verbose: true,
     hooks: [
       pre_commit: [
         tasks: [
-          {:cmd, "mix format --check-formatted"},
-          {:cmd, "echo 'mix format: ok!'"}
+          {:cmd, "mix format --check-formatted"}
         ]
       ],
       pre_push: [
         verbose: false,
         tasks: [
-          {:cmd, "mix test"},
-          {:cmd, "echo 'mix test: success!'"}
+          {:cmd, "mix test"}
         ]
       ]
     ]
