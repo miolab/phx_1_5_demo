bc16c4145fac478cecbcf2cbef0b6f61d84a7b35
im <poson_hemp@yahoo.co.jp>
2020-12-28 17:46:17 +0900

    run: mix format & mix test

--- config/config.exs
+++ config/config.exs
@@ -28,19 +28,19 @@ config :phoenix, :json_library, Jason
 import_config "#{Mix.env()}.exs"
 
 # Set git_hooks
-  config :git_hooks,
-    auto_install: true,
-    verbose: true,
-    hooks: [
-      pre_commit: [
-        tasks: [
-          {:cmd, "mix format --check-formatted"}
-        ]
-      ],
-      pre_push: [
-        verbose: false,
-        tasks: [
-          {:cmd, "mix test"}
-        ]
+config :git_hooks,
+  auto_install: true,
+  verbose: true,
+  hooks: [
+    pre_commit: [
+      tasks: [
+        {:cmd, "mix format --check-formatted"}
+      ]
+    ],
+    pre_push: [
+      verbose: false,
+      tasks: [
+        {:cmd, "mix test"}
       ]
     ]
+  ]
